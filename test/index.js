c8c07d39f64f69205d5e011bcde127139b1a9a88
yaochenghao <yaochenghao@bytedance.com>
2018-01-07 11:21:11 +0800

    upd: mocha test case

--- test/index.js
+++ test/index.js
@@ -536,10 +536,6 @@ describe('lib-match', function() {
     });
 
     describe('match config', function() {
-        // miss some case here
-        // TODO filterEmptyObject
-        // TODO filterDefaultArray
-        // TODO filterDefaultObject
         it('filter undefined', function() {
             params = {
                 pid: 1,
@@ -645,6 +641,123 @@ describe('lib-match', function() {
                 pid: 1,
             });
         });
+        it('filter empty object in result', function() {
+            params = {
+                name: {
+                    id: 1,
+                },
+            };
+            data = match
+                .tmpConfig({
+                    filterEmptyObject: true,
+                })
+                .parse(params, {
+                    title: '$${{abc}}',
+                    data: {
+                        id: '$${{name.id}}',
+                        value: {
+                            id: '$${{name.title}}',
+                        },
+                    },
+                    text: {
+                        title: '$${{name.abc}}',
+                    },
+                    name: '$${{name.value}} || {}',
+                });
+            assert.deepEqual(data, {
+                data: {
+                    id: 1,
+                },
+            });
+        });
+
+        it('filter default array in result(keep the array with || expression)', function() {
+            params = null;
+            data = match
+                .tmpConfig({
+                    filterDefaultArray: true,
+                })
+                .parse(params, [
+                    {
+                        id: '$${{id}}',
+                        title: 'string',
+                        type: "$${{type}} || 'abc'",
+                    },
+                ]);
+            assert.deepEqual(data, undefined);
+
+            params = {};
+            data = match
+                .tmpConfig({
+                    filterDefaultArray: true,
+                })
+                .parse(params, [
+                    'data',
+                    {
+                        id: '$${{id}}',
+                        title: 'string',
+                        type: "$${{type}} || 'abc'",
+                    },
+                ]);
+            assert.deepEqual(data, undefined);
+
+            params = {
+                code: 200,
+                msg: 'ok',
+                data: null,
+            };
+            data = match
+                .tmpConfig({
+                    filterDefaultArray: true,
+                })
+                .parse(params, {
+                    code: '$${{code}}',
+                    msg: '$${{msg}}',
+                    array: '$${{array}} || []',
+                    data: [
+                        'data',
+                        {
+                            id: '$${{id}}',
+                            title: 'string',
+                            type: "$${{type}} || 'abc'",
+                        },
+                    ],
+                });
+            assert.deepEqual(data, {
+                code: 200,
+                msg: 'ok',
+                array: [],
+            });
+        });
+
+        it('filter default object in result(keep the object with || expression)', function() {
+            params = {
+                name: {
+                    id: 1,
+                },
+            };
+            data = match
+                .tmpConfig({
+                    filterDefaultObject: true,
+                })
+                .parse(params, {
+                    data: {
+                        xx: '$${{name.title}}',
+                        value: {
+                            id: '$${{name.title}}',
+                        },
+                        yy: '$${{name.title}}',
+                    },
+                    data2: {
+                        value2: '$${{name.title}} || {}',
+                    },
+                });
+            assert.deepEqual(data, {
+                data2: {
+                    value2: {},
+                },
+            });
+        });
     });
     describe('match multi input', function() {
         it('the match input be a Array', function() {
@@ -675,7 +788,7 @@ describe('lib-match', function() {
             });
         });
     });
-    describe('match and trans the type', function() {
+    describe('match and type conversion', function() {
         it('int string float boolean', function() {
             params = {
                 pid: 'false',
